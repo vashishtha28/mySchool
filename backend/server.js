d2237d4963a828198ef69ca175e808a3d1facbe5
vashishtha28 <vashishthaflutter@gmail.com>
2021-04-05 17:55:22 +0530

    passport integrated

--- backend/server.js
+++ backend/server.js
@@ -1,58 +1,150 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require("cors");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const app = express();
 const port = 5000;
-const mongoose = require("mongoose");
-//This is a trial change
+//______________________________________App.use___________________________________________________
+app.use(express.json());
+app.use(cors());
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //______________________________________DATABASE Setup_____________________________________________
 //Set up default mongoose connection
 var mongoDB = process.env.DB_URL;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
 //Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-//_________________________________________________________________________________________________
 
 
+//________________________________________ALL SCHEMAS______________________________________________
 var Schema = mongoose.Schema;
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
 
-//Test data
-const regSchools = ["KV IITG", "DPS Rajnagar"];
-const students =[ {
-  schoolName: "KV IITG",
-  email: "student@gmail.com",
-  password: "123456"
-}];
+userSchema.plugin(passportLocalMongoose);
+const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.use(express.json());
-app.use(cors());
+const attendanceSchema = new mongoose.Schema({
+  date: Date,
+  isPresent: Boolean,
+});
+
+const classSchema = new mongoose.Schema({
+  class: String,
+  section: String
+});
+
+const noticeSchema = new mongoose.Schema({
+  date: Date,
+  title: String,
+  content: String,
+  sender: String
+});
+
+const testResultSchema = new mongoose.Schema({
+  date: Date,
+  subject: String,
+  testDescription: String,
+  maxMarks: String,
+  marksObtained: String
+});
+
+const studentSchema = new mongoose.Schema({
+  username: String,
+  role: String,
+  //password: String,
+  studentName: String,
+  class: String,
+  section: String,
+  rollNum: String,
+  fatherName: String,
+  motherName: String,
+  parentEmailId: String,
+  attendanceList: [attendanceSchema],
+  gradeCard: [testResultSchema],
+  notices: [noticeSchema]
+});
+
+const teacherSchema = new mongoose.Schema({
+  username: String,
+  role: String,
+  //password: String,
+  teacherName: String,
+  mobileNum: String,
+  subjects: [String],
+  classes: [classSchema],
+  classTeacherOf: classSchema,
+  notices: [noticeSchema]
+});
+
+//_____________________________________________app.get()____________________________________
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to the mySchool backend</h1>')
 });
 
 //___________________________________________________________________________________________
 
-//currently we are not checking for roles
-//need proper database for that
-app.post("/login", function(req, res){
-  const result = students.find(user => user.email == req.body.email);
-  if(result){
-      if(result.password ==req.body.password){
-          res.send({
-              message: "Successful login"
-          });
-      }else{
-          res.send({message: "Password Incorrect"});
-      }
+//MANUALLY ADDED SOME USERS
 
-  }else{
-      res.send({message: "user not found"});
-  }
-  res.send({message: "authenticated dude"})
+// User.register({username:"Admin" }, "123456" , function(err, user){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log("Successfully Registered ");
+//   }
+// });
+
+//__________________________________________________app.post()__________________________________________________
+
+
+app.post("/login", function(req, res){
+  console.log(req.body);
+  
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+    req.login(user, function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        passport.authenticate("local")(req, res, function(){
+          console.log("Admin logged in.");
+        });//after authentication, send userInfo as a response.
+      }
+    });
 });
+
+app.post("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+  //send confirmation to frontend and redirect to homepage.
+});
+
+
+
 
 app.listen(port, () => {
   console.log("Server running on port ", port);
