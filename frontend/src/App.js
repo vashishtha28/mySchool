44f6375a5fea7b56eda84102c601d72fcae83ed6
vashishtha28 <vashishthaflutter@gmail.com>
2021-04-07 01:04:29 +0530

    trial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import SignIn from "./pages/SignIn";
 import MyAppBar from "./components/MyAppBar"
 import RegisterStudent from "./pages/RegisterStudent";
-import RegisterTeacher from "./pages/RegisterTeacher";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <SignIn /> */}
-      {/* <RegisterStudent/> */}
-      <RegisterTeacher/>
+     <SignIn />
     </div>
   );
 }
