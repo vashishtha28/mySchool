56314408fe8e813905c90d30e35fa4d85463942f
vsinha312 <sinha.vatsal123@gmail.com>
2021-04-07 00:55:52 +0530

    App.js changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import SignIn from "./pages/SignIn";
 import MyAppBar from "./components/MyAppBar"
 import RegisterStudent from "./pages/RegisterStudent";
-import AdminPage from "./pages/AdminWelcome"
+
 
 function App() {
   return (
     <div className="App">
-     {/*<SignIn />*/}
-      {/* <RegisterStudent/> */}
-      <AdminPage />
+     <SignIn />
     </div>
   );
 }
