aec926b09e4a9aaa6a83f4a9baa36341bfd7b8af
Curious-Ujjwal <ujjwal24111999@gmail.com>
2021-04-09 00:23:16 +0530

    Added genNotice & studentAttendance

---
+++ frontend/src/pages/calendar.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import './styles.css';
+import './calendarStyles.css';
+import moment from 'moment';
+
+export default function Calendar() {
+    const [selectedDate, setSelectedDate] = useState(moment());
+
+    const [calendar, setCalendar] = useState([]);
+    const [value, setValue] = useState(moment());
+
+    const startDay = value.clone().startOf("month").startOf("week");
+    const endDay = value.clone().endOf("month").endOf("week");
+
+    useEffect(() => {
+        const day = startDay.clone().subtract(1, "day");
+        const a = [];
+        while(day.isBefore(endDay, "day")) {
+            a.push(
+                Array(7)
+                .fill(0)
+                .map(() => day.add(1, "day").clone())
+            )
+        }
+        setCalendar(a);
+    }, [value])
+
+    function isSelected(day) {
+        return value.isSame(day, "day")
+    }
+
+// function to check whether a student is present or not
+
+    // function isPresent(day) {
+    //     return schema[day].isPresent;
+    // }
+
+    function beforeToday(day) {
+        return day.isBefore(new Date(), "day");
+    }
+
+    function isToday(day) {
+        return day.isSame(new Date(), "day");
+    }
+
+    function dayStyles(day) {
+        if (beforeToday(day))   return "before";
+        if (isSelected(day))    return "selected";
+        if(isToday(day))    return "today";
+        return "";
+    }
+
+    function currentMonthName() {
+        return value.format("MMMM");
+    }
+
+    function currentYear() {
+        return value.format("YYYY");
+    }
+
+    function prevMonth() {
+        return value.clone().subtract(1, "month");
+    }
+
+    function nextMonth() {
+        return value.clone().add(1, "month");
+    }
+
+    return (
+            <div className="calendar">
+                <div className="header">
+                    <div className="previous" onClick={() => setValue(prevMonth())} style={{ cursor: 'pointer' }}>{ String.fromCharCode(171) }</div>
+                    <div className="current"> { currentMonthName() } { currentYear() } </div>
+                    <div className="next" onClick={() => setValue(nextMonth())} style={{ cursor: 'pointer' }}>{ String.fromCharCode(187) }</div>
+                </div>
+                <div className="body">
+                    <div className="day-names">
+                        {
+                            ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map(d => 
+                                <div className="week">{d}</div>
+                                )
+                        }
+                    </div>
+                    {
+                        calendar.map(week => <div>
+                            { 
+                                week.map(day => <div className="day" onClick={() => setValue(day)}>
+                                    <div className={ dayStyles(day) } style={{ cursor: 'pointer' }}>
+                                        {day.format("D")}
+                                    </div>
+                                </div>)
+                            }
+                        </div>)
+                    }
+                </div>
+            </div>
+        );
+}
---
+++ frontend/src/pages/genNotice.js
@@ -0,0 +1,137 @@
+import React, {useState, useEffect, Component} from 'react';
+import axios from "axios";
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import MyAppBar from '../components/MyAppBar';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import { InputLabel, makeStyles } from '@material-ui/core';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+    form: {
+        width: '40%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        width: '100%',
+    }
+}));
+
+export default function GenerateNotice() {
+    const [clas, setClas] = useState('');
+    const [section, setSection] = useState('');
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+
+    const [date, setDate] = useState(today);
+    const classes = useStyles();
+
+    function handleClassChange(event) {
+        event.preventDefault();
+        setClas(event.target.value);
+    }
+
+    function handleSectionChange(event) {
+        event.preventDefault();
+        setSection(event.target.value);
+    }
+
+    const handleDateChange = (date) => {
+        setDate(date);
+    };
+
+    return (
+        <div>
+            <MyAppBar appBarTitle="Generate Notice"/>
+            <form className={classes.formControl} noValidate style={{ marginTop: '0px', marginLeft: '0px' }}>
+                <Box style={{ backgroundColor: "#00ADB5", padding: '3.679890560875513vh', }}>
+                    <Grid container spacing={3} style={{ justifyContent: 'space-around' }}>
+                        <Grid item xs={12} sm={4}>
+                            <FormControl variant="filled" className={classes.form}>
+                                <InputLabel id="simple-class-filled-label">Class</InputLabel>
+                                <Select labelId="simple-class-filled-label" id="dsimple-class-filled" value={clas} onChange={handleClassChange} style={{ textAlign: 'left'}} >
+                                    <MenuItem value={"1"}>1</MenuItem>
+                                    <MenuItem value={"2"}>2</MenuItem>
+                                    <MenuItem value={"3"}>3</MenuItem>
+                                    <MenuItem value={"4"}>4</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <FormControl variant="filled" className={classes.form}>
+                                <InputLabel id="simple-section-filled-label">Section</InputLabel>
+                                <Select labelId="simple-section-filled-label" id="dsimple-section-filled" value={section} onChange={handleSectionChange} style={{ textAlign: 'left' }}>
+                                    <MenuItem value={"A"}>A</MenuItem>
+                                    <MenuItem value={"B"}>B</MenuItem>
+                                    <MenuItem value={"C"}>C</MenuItem>
+                                    <MenuItem value={"D"}>D</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <MuiPickersUtilsProvider utils={DateFnsUtils} className={classes.formControl}>
+                                <KeyboardDatePicker
+                                // disableToolbar
+                                variant="inline"
+                                format="dd/MM/yyyy"
+                                margin="normal"
+                                id="date-picker-inline"
+                                label="Date"
+                                value={date}
+                                onChange={handleDateChange}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change date',
+                                }}
+                                className={classes.form}
+                                />
+                            </MuiPickersUtilsProvider>
+                        </Grid>
+                    </Grid>
+                </Box>
+                <Container maxWidth="lg" style={{ marginTop: '12px', maarginBottom: '12px' }}>
+                    <Grid container spacing={3} style={{ justifyContent: 'space-around' }}>
+                        <Grid item xs={12}>
+                            <TextField id="filled-basic" label="Notice Title" variant="filled" style={{ width: '100%' }}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextareaAutosize aria-label="Body" placeholder="Notice Body" style={{ backgroundColor: '#DCDCDC', fontSize: '16px', padding: '16px', border: 'none', minWidth: '78vw', minHeight: '40vh' }}/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Button variant="contained" style={{ backgroundColor: '#E96565', width: '50%', color: 'white' }}>
+                                Discard
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Button variant="contained" style={{ backgroundColor: '#00B594', width: '50%', color: 'white' }}>
+                                Send
+                            </Button>                   
+                        </Grid>
+                    </Grid>
+                </Container>
+            </form>
+            
+        </div>
+    );
+}
